84cc20a222ee689eea368c52f28421ae10414f68
johan-105 <672016243@qq.com>
2023-02-06 11:04:24 +0800

    添加对MethodArgumentNotValidException的处理

--- common/src/main/java/com/johann/mall/common/exception/RRExceptionHandler.java
+++ common/src/main/java/com/johann/mall/common/exception/RRExceptionHandler.java
@@ -12,9 +12,14 @@ import com.johann.mall.common.utils.R;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 异常处理器
@@ -35,6 +40,18 @@ public class RRExceptionHandler {
 		r.put("msg", e.getMessage());
 
 		return r;
+	}
+
+	@ExceptionHandler(value= MethodArgumentNotValidException.class)
+	public R handleVaildException(MethodArgumentNotValidException e){
+		logger.error("数据校验出现问题{}，异常类型：{}",e.getMessage(),e.getClass());
+		BindingResult bindingResult = e.getBindingResult();
+
+		Map<String,String> errorMap = new HashMap<>();
+		bindingResult.getFieldErrors().forEach((fieldError)->{
+			errorMap.put(fieldError.getField(),fieldError.getDefaultMessage());
+		});
+		return R.error(400, "数据校验出现问题").put("data",errorMap);
 	}
 
 	@ExceptionHandler(NoHandlerFoundException.class)
